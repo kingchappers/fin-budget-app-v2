07970df3bd7e3bdf92d439a2b62a4f464384a160
Sam Chapman <sam.chapman@outlook.com>
2024-06-29 14:09:43 +0100

    Added tailwindcss to project

--- app.vue
+++ app.vue
@@ -1,6 +1,17 @@
+<script setup lang="ts">
+const version = 2 + 1
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+    <div class="text-3xl font-bold underline">
+        Hello Nuxt {{ version }}!
+    </div>
 </template>
+
+<style scoped>
+.hello {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 3rem;
+    padding: 2rem;
+}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./components/**/*.{js,vue,ts}",
+        "./layouts/**/*.vue",
+        "./pages/**/*.vue",
+        "./plugins/**/*.{js,ts}",
+        "./app.vue",
+        "./error.vue",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic':
+                    'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            },
+        },
+    },
+    plugins: [],
+}
+
