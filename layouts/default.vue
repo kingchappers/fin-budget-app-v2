fa0cd7c5877104c6fef7c2f955167c7264c86117
Sam Chapman <sam.chapman@outlook.com>
2024-06-29 15:53:20 +0100

    added default layout

---
+++ layouts/default.vue
@@ -0,0 +1,37 @@
+<template>
+    <html lang="en" class="text-xs lg:text-lg container justify-center mx-auto p-4 bg-sky-100">
+    <div>
+
+        <head>
+            <link rel="icon" href="/favicon.ico" sizes="any" />
+        </head>
+
+        <body>
+            <header>
+                <div class="flex">
+                    <img src="/images/fin-wide.png" width={100} height={100} alt="Fin Logo" class="mr-3" />
+                    <ul class="flex mt-5 lg:text-xl">
+                        <li class="lg:mx-5 mx-2">
+                            <p>Budget Menu Placeholder</p><!-- <BudgetMenu />--->
+                        </li>
+                        <li class="lg:mx-5 mx-2">
+                            <p>Account Menu Placeholder</p><!-- <AccountMenu /> -->
+                        </li>
+                        <li class="lg:mx-5 mx-2">
+                            <p>Utilities Placeholder</p>
+                        </li>
+                        <li class="lg:mx-5 mx-2">
+                            <p>Help Placeholder</p>
+                        </li>
+                    </ul>
+                </div>
+            </header>
+
+            <main class="py-5 lg:py-10">
+                <slot />
+            </main>
+        </body>
+    </div>
+
+    </html>
+</template>
