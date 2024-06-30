fa3adca686bbc7cc142602da4222f3e50365c9ff
Sam Chapman <sam.chapman@outlook.com>
2024-06-30 17:00:00 +0100

    Redesigned default layout with new menu structure

---
+++ components/menus/menusTopNavigation.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+const route = useRoute()
+
+const links = [
+    [{
+        label: 'Transactions',
+        icon: 'i-heroicons-credit-card',
+        to: '/transactions'
+    }, {
+        label: 'Income',
+        icon: 'i-heroicons-briefcase',
+        to: '/income'
+    }, {
+        label: 'Categories',
+        icon: 'i-heroicons-tag',
+        to: `/category`
+    }, {
+        label: 'Targets',
+        icon: 'i-heroicons-chart-pie',
+        to: '/targets'
+    }, {
+        label: 'Variance',
+        icon: 'i-heroicons-chart-bar',
+        to: '/variance'
+    }, {
+        label: 'Savings',
+        icon: 'i-heroicons-currency-pound',
+        to: '/savings'
+    }, {
+        label: 'Trends',
+        icon: 'i-heroicons-presentation-chart-line',
+        to: '/trends'
+    }, {
+        label: 'Utilties',
+        icon: 'i-heroicons-wrench-screwdriver',
+        to: '/utilities'
+    }], [{
+        label: 'Sign In / Register',
+        icon: 'i-heroicons-arrow-left-end-on-rectangle',
+        to: '/profile'
+    }, {
+        label: 'Sign Out',
+        icon: 'i-heroicons-arrow-left-start-on-rectangle',
+        to: '/profile'
+    }, {
+        label: 'Profile',
+        icon: 'i-heroicons-user',
+        to: '/profile'
+    }, {
+        label: 'Help',
+        icon: 'i-heroicons-question-mark-circle',
+        to: '/help'
+    }]
+]
+</script>
+
+<template>
+    <UHorizontalNavigation :links="links" class="border-b border-gray-200 dark:border-gray-800" />
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -9,21 +9,9 @@
         <body>
             <header>
                 <div class="flex">
-                    <NuxtLink to="/"><img src="/images/fin-wide.png" width={100} height={100} alt="Fin Logo" class="mr-3" /></NuxtLink>
-                    <ul class="flex mt-5 lg:text-xl">
-                        <li class="lg:mx-5 mx-2">
-                            <p>Budget Menu Placeholder</p><!-- <BudgetMenu />--->
-                        </li>
-                        <li class="lg:mx-5 mx-2">
-                            <p>Account Menu Placeholder</p><!-- <AccountMenu /> -->
-                        </li>
-                        <li class="lg:mx-5 mx-2">
-                            <NuxtLink href="/utilities">Utilities</NuxtLink>
-                        </li>
-                        <li class="lg:mx-5 mx-2">
-                            <NuxtLink href="/help">Help</NuxtLink>
-                        </li>
-                    </ul>
+                    <NuxtLink to="/"><NuxtImg src="/images/fin-wide.png" width="75" height="50" alt="Fin Logo"
+                            class="mr-3" /></NuxtLink>
+                    <MenusTopNavigation />
                 </div>
             </header>
 
