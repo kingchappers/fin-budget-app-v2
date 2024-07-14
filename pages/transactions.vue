6c944a1b787f0351b9969091e1a924f3f4ed884d
Sam Chapman <sam.chapman@outlook.com>
2024-07-14 17:14:21 +0100

    Added parameter options for api route

--- pages/transactions.vue
+++ pages/transactions.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
-const { data: transactions } = await useFetch('/api/transactions/getTransactions')
+
+const transactionFilter = {
+    limit: 5,
+    page: 0
+}
+
+const { data: transactions } = await useFetch('/api/transactions/getTransactions', {
+    method: 'get',
+    params: {
+        transactionFilter
+    },
+})
 </script>
 
 <template>
     <main>
         <div>
             <h1 class="text-4xl font-extrabold">Transactions Placeholder</h1>
-            <p>Test: {{ transactions }}</p>
+            <p>transactions: {{ transactions }}</p>
         </div>
     </main>
 </template>
--- server/api/transactions/getTransactions.ts
+++ server/api/transactions/getTransactions.ts
@@ -1,7 +1,26 @@
 import { Transaction } from "~/server/models/transaction.model";
+import { z } from 'zod'
+
+const transactionFilter = z.object({
+    page: z.number().default(1),
+    limit: z.number().default(50),
+    // userId: string;
+})
 
 export default defineEventHandler(async (event) => {
-    const transactions = await Transaction.find();
+    //Validate the passed parameters
+    const params = await getValidatedQuery(event, data => transactionFilter.safeParse(data))
+
+    if (!params.success) {
+        throw params.error.issues
+    }
+
+    const limit = params.data.limit
+    const page = params.data.page;
+    const skip = (page - 1) * limit;
+
+    const transactions = await Transaction.find().sort({ transactionDate: -1 }).skip(skip).limit(limit).lean().exec();;
+
 
     return transactions;
 });
