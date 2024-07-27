107861e7f1bc04b9fed72cd4b2b95c468dd2ca6f
Sam Chapman <sam.chapman@outlook.com>
2024-07-27 14:28:03 +0100

    updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: { enabled: true },
-    css: ['~/assets/css/main.css'],
-    postcss: {
-        plugins: {
-            tailwindcss: {},
-            autoprefixer: {},
-        }
-    },
-    nitro: {
-        plugins: ["~/server/plugins/mongodb.ts"],
-    },
-    runtimeConfig: {
-        mongodbUri:'',
-    },
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
 
-    modules: ["@nuxt/ui", "@nuxt/image"]
+  postcss: {
+      plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+      }
+  },
+
+  nitro: {
+      plugins: ["~/server/plugins/mongodb.ts"],
+  },
+
+  runtimeConfig: {
+      mongodbUri:'',
+  },
+
+  modules: ["@nuxt/ui", "@nuxt/image"],
+  compatibilityDate: "2024-07-27"
 })
