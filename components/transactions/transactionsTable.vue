2fe68c8dddbb39899784583e18f87b10bffd368a
Sam Chapman <sam.chapman@outlook.com>
2024-08-10 22:05:25 +0100

    added single delete button

--- components/transactions/transactionsTable.vue
+++ components/transactions/transactionsTable.vue
@@ -11,8 +11,7 @@ const columns = [{
     key: 'transactionDate',
     label: 'Date',
     sortable: true
-},
-{
+}, {
     key: 'vendor',
     label: 'Vendor'
 }, {
@@ -27,7 +26,21 @@ const columns = [{
 }, {
     key: 'notes',
     label: 'Notes'
+}, {
+    key: 'actions'
 }]
+
+const items = (row: transactionType) => [
+    [{
+        label: 'Edit',
+        icon: 'i-heroicons-pencil-square-20-solid',
+        click: () => console.log('Edit', row.transactionDate)
+    },], [{
+        label: 'Delete',
+        icon: 'i-heroicons-trash-20-solid',
+        click: () => deleteTransactions([row])
+    }]
+]
 const selectedValues = ref([])
 
 async function deleteTransactions(selectedValues: transactionType[]) {
@@ -56,17 +69,28 @@ async function deleteTransactions(selectedValues: transactionType[]) {
     <div v-if="transactionsList">
         <div class="flex flex-row space-x-4">
             <UButton icon="i-heroicons-trash" class="bg-red-600 hover:bg-red-700"
-                @click="deleteTransactions(selectedValues)">Delete</UButton>
+                @click="deleteTransactions(selectedValues)">Delete Selected</UButton>
         </div>
         <UTable :empty-state="{ icon: 'i-heroicons-circle-stack-20-solid', label: 'No items.' }"
             v-model="selectedValues" :columns="columns" :rows="transactionsList.transactions">
             <template #value-data="{ row }">
                 <p>£{{ row.value }}</p>
             </template>
+
             <template #transactionDate-data="{ row }">
                 <p>{{ format(row.transactionDate, 'dd/MM/yyy') }}</p>
+            </template>
+
+            <template #actions-data="{ row }">
+                <UDropdown :items="items(row)">
+                    <UButton color="gray" variant="ghost" icon="i-heroicons-ellipsis-horizontal-20-solid" />
+                </UDropdown>
             </template>
             <!-- Implement a menu here for editing? Would not work with current versions of nuxtui -->
         </UTable>
     </div>
 </template>
+
+<!-- label: 'Edit',
+    icon: 'i-heroicons-pencil-square-20-solid',
+    click: () => console.log('Edit', row.id) -->
