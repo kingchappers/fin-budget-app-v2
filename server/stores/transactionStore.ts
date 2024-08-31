28e8c1a3f84b1cf117f8a30789211bc391b24a49
Sam Chapman <sam.chapman@outlook.com>
2024-08-31 21:05:59 +0100

    removed redundant line

--- server/stores/transactionStore.ts
+++ server/stores/transactionStore.ts
@@ -1,5 +1,4 @@
 import { defineStore } from "pinia";
-import { ref } from "vue";
 import type { transactionFilter } from '~/types/transactionFilter';
 import type { transactionType } from "~/types/transactionTypes";
 
