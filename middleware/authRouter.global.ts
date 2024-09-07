04b9b7554b53a64d65ea14079d032883fd4bfd12
Sam Chapman <sam.chapman@outlook.com>
2024-09-07 13:14:23 +0100

    middleware is now global and works for unauthenticated users

--- middleware/authRouter.global.ts
+++ middleware/authRouter.global.ts
@@ -1,11 +1,17 @@
 import { useAuthenticator } from '@aws-amplify/ui-vue';
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
+    // skip middleware on initial client load
+    const nuxtApp = useNuxtApp()
+    if (import.meta.client && nuxtApp.isHydrating && nuxtApp.payload.serverRendered)
+        return
+
     const auth = useAuthenticator()
     const publicPages = ['/login', '/'];
     const authRequired = !publicPages.includes(to.path);
 
-    if (authRequired && auth.authStatus === 'authenticated') {
+    if (authRequired && auth.authStatus !== 'authenticated') {
         return navigateTo('/login')
     }
+    console.log(auth.authStatus)
 })
