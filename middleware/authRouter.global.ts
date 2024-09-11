920b3b59d4e734977d6c777df08624f357658a23
Sam Chapman <sam.chapman@outlook.com>
2024-09-11 21:39:07 +0100

    updated authrouter to get the userid

--- middleware/authRouter.global.ts
+++ middleware/authRouter.global.ts
@@ -1,4 +1,9 @@
+import { getCurrentUser } from '@aws-amplify/auth';
 import { useAuthenticator } from '@aws-amplify/ui-vue';
+import { useUserStore } from '~/server/stores/userStore';
+import { Amplify } from 'aws-amplify';
+import awsconfig from '../src/aws-exports';
+Amplify.configure(awsconfig);
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
     // skip middleware on initial client load
@@ -6,12 +11,18 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
     if (import.meta.client && nuxtApp.isHydrating && nuxtApp.payload.serverRendered)
         return
 
-    const auth = useAuthenticator()
+    const auth = await useAuthenticator()
     const publicPages = ['/login', '/'];
     const authRequired = !publicPages.includes(to.path);
 
     if (authRequired && auth.authStatus !== 'authenticated') {
         return navigateTo('/login')
     }
-    console.log(auth.authStatus)
+
+    if (auth.authStatus == 'authenticated') {
+        const { userId } = await getCurrentUser()
+        const userStore = useUserStore();
+        userStore.initStore(userId)
+    }
+
 })
