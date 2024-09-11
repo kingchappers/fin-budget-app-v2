674a2352ff319361cc65ba9d6186bcdf6f5e3199
Sam Chapman <sam.chapman@outlook.com>
2024-09-11 21:37:45 +0100

    added user ID to transaction store

--- server/stores/transactionStore.ts
+++ server/stores/transactionStore.ts
@@ -1,11 +1,22 @@
+import { fetchAuthSession } from "@aws-amplify/auth";
 import { defineStore } from "pinia";
 import type { transactionFilter } from '~/types/transactionFilter';
 import type { transactionType } from "~/types/transactionTypes";
+import { useUserStore } from "./userStore";
+
+const session = await fetchAuthSession();
+const userStore = useUserStore();
+let authorisation = ''
+if (session.tokens && session.tokens.idToken) {
+    authorisation = session.tokens.idToken.toString()
+} else {
+    console.log('Error: Session token not found. Redirecting to login')
+}
 
 const transactionFilter: transactionFilter = {
     limit: 5,
     page: 0,
-    userId: ''
+    userId: userStore.userId
 }
 
 export const useTransactionStore = defineStore('transactionStore', {
@@ -13,12 +24,17 @@ export const useTransactionStore = defineStore('transactionStore', {
         return {
             transactionsList: [] as object as transactionsList,
             status: '',
+            userId: userStore.userId,
         }
     },
     actions: {
         async fetch() {
             const transactionList = await $fetch('/api/transactions/getTransactions', {
                 method: 'get',
+                headers: {
+                    Authorisation: authorisation,
+                    UserId: this.userId
+                },
                 params: {
                     transactionFilter
                 },
