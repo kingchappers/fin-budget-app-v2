2b12cceecb2c815bd00a5d6c1f5378523854145a
Sam Chapman <sam.chapman@outlook.com>
2024-09-22 17:34:31 +0100

    updated referecnce

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <script setup>
 import "@aws-amplify/ui-vue/styles.css";
 import { Amplify } from 'aws-amplify';
-import awsconfig from '~/aws-exports';
+import awsconfig from '~/src/aws-exports.js';
 
 Amplify.configure(awsconfig);
 </script>
--- pages/login.vue
+++ pages/login.vue
@@ -2,7 +2,7 @@
 import { Authenticator, useAuthenticator } from "@aws-amplify/ui-vue";
 import "@aws-amplify/ui-vue/styles.css";
 import { Amplify } from 'aws-amplify';
-import awsconfig from '~/src/aws-exports';
+import awsconfig from '~/src/aws-exports.js';
 
 Amplify.configure(awsconfig);
 const auth = useAuthenticator()
