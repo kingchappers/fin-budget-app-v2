5781efeba1bac00980c0f2b4ab78bc0e897679e6
Sam Chapman <sam.chapman@outlook.com>
2024-09-22 16:56:58 +0100

    updated aws exports import statement

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
 import "@aws-amplify/ui-vue/styles.css";
 
 import { Amplify } from 'aws-amplify';
-import awsconfig from './src/aws-exports';
+import awsconfig from '~/src/aws-exports';
 
 Amplify.configure(awsconfig);
 </script>
