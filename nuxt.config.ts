d6db1b3d89836b84027dbe3f0df5142ec8f62cbf
Sam Chapman <sam.chapman@outlook.com>
2024-09-22 16:13:03 +0100

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    //   devtools: { enabled: true }, test
+    //   devtools: { enabled: true }, 
     css: ['~/assets/css/main.css'],
 
     postcss: {
