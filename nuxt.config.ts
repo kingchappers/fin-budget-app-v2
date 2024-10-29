6286f7d529c9f5bd4086a666a75cb949f39c843e
Sam Chapman <sam.chapman@outlook.com>
2024-10-29 18:32:46 +0000

    removed bad plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineNuxtConfig({
     //   devtools: { enabled: true },
