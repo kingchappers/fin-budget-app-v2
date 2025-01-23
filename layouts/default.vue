edfa9794f4c24e3e0a0d71b17d8025a768968b2c
Samuel Chapman <samuelchapman@Samuels-MacBook-Air.local>
2025-01-23 21:41:50 +0000

    updated text colour and dom model

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,26 +1,25 @@
 <template>
-    <div class="bg-sky-300">
-        <html lang="en" class="text-xs lg:text-lg container justify-center mx-auto p-4 h-screen bg-sky-100">
-            <head>
-                <link rel="icon" href="/favicon.ico" sizes="any" />
-            </head>
+    <html lang="en" class="bg-sky-300 text-slate-800">
 
-            <body>
-                <header>
-                    <div class="flex">
-                        <NuxtLink to="/">
-                            <NuxtImg src="/images/fin-wide.png" width="75" height="50" alt="Fin Logo" class="mr-3" />
-                        </NuxtLink>
-                        <MenusTopNavigation />
-                    </div>
-                </header>
+        <head>
+            <link rel="icon" href="/favicon.ico" sizes="any" />
+        </head>
 
-                <main class="py-5 lg:py-10">
-                    <slot />
-                </main>
-            </body>
+        <body class="text-xs lg:text-lg container justify-center mx-auto p-4 h-screen bg-sky-100">
+            <header>
+                <div class="flex">
+                    <NuxtLink to="/">
+                        <NuxtImg src="/images/fin-wide.png" width="75" height="50" alt="Fin Logo" class="mr-3" />
+                    </NuxtLink>
+                    <MenusTopNavigation />
+                </div>
+            </header>
 
-        </html>
-    </div>
+            <main class="py-5 lg:py-10">
+                <slot />
+            </main>
+        </body>
+
+    </html>
 
 </template>
