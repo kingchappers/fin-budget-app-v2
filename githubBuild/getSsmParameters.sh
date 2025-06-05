b48622f26ee2f8cf642e676a929b45186371d947
kingchappers <sam.chapman@outlook.com>
2025-06-05 17:52:27 -0400

    changed to get parameter commands

--- githubBuild/getSsmParameters.sh
+++ githubBuild/getSsmParameters.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-aws ssm put-parameter --name "COGNITO_USER_POOL_ID" --value $COGNITO_USER_POOL_ID --type String
-aws ssm put-parameter --name "COGNITO_USER_POOL_CLIENT_ID" --value $COGNITO_USER_POOL_CLIENT_ID --type String
-aws ssm put-parameter --name "COGNITO_IDENTITY_POOL_ID" --value $COGNITO_IDENTITY_POOL_ID --type String
+userPoolId=$(aws ssm get-parameter --name /amplify/fin-budget/prod/COGNITO_USER_POOL_ID | jq '.Parameter.Value')
+userPoolClientId=$(aws ssm get-parameter --name /amplify/fin-budget/prod/COGNITO_USER_POOL_CLIENT_ID | jq '.Parameter.Value')
+identityPoolId=$(aws ssm get-parameter --name /amplify/fin-budget/prod/COGNITO_IDENTITY_POOL_ID | jq '.Parameter.Value')
