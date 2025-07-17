1952a5019a815f5c14fa33227fc14681b3d7db67
kingchappers <sam.chapman@outlook.com>
2025-07-17 07:51:47 +0100

    another attempt

--- githubBuild/getSsmParameters.sh
+++ githubBuild/getSsmParameters.sh
@@ -2,3 +2,4 @@
 userPoolId=$(aws ssm get-parameter --name /amplify/d3m9wu6rhd9z99/main/AMPLIFY_COGNITO_USER_POOL_ID | jq '.Parameter.Value')
 userPoolClientId=$(aws ssm get-parameter --name /amplify/fin-budget/prod/COGNITO_USER_POOL_CLIENT_ID | jq '.Parameter.Value')
 identityPoolId=$(aws ssm get-parameter --name /amplify/fin-budget/prod/COGNITO_IDENTITY_POOL_ID | jq '.Parameter.Value')
+AMPLIFY_TEST=$(aws ssm get-parameter --name AMPLIFY_TEST | jq '.Parameter.Value')
--- pages/login.vue
+++ pages/login.vue
@@ -12,9 +12,9 @@ import { Amplify } from 'aws-amplify';
 // https://ui.docs.amplify.aws/vue/connected-components/authenticator/customization#headers--footers
 // if (process.env.NUXT_PUBLIC_AMPLIFY_TEST === undefined) {
 if (process.env.VITE_AMPLIFY_TEST === undefined) {
-  console.log(process.env._LIVE_PACKAGE_UPDATES)
-  console.log("AMPLIFY_TEST", process.env.VITE_AMPLIFY_TEST);
-  console.log("variable", process.env.BROWSERSLIST_ENV)
+  console.log("AMPLIFY_TEST", process.env.AMPLIFY_TEST);
+  console.log("New Amplify_test", process.env.AMPLIFY_TEST);
+  console.log("VITE_AMPLIFY_TEST", ref(import.meta.env.VITE_AMPLIFY_TEST));
   throw new Error("Missing TEST environment variable");
 } else {
   console.log("TEST environment variable is set to:", process.env.VITE_AMPLIFY_TEST);
