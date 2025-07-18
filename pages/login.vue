c75340633aa37fcbd8e2555b7320719aba3411e8
kingchappers <sam.chapman@outlook.com>
2025-07-18 14:40:43 +0100

    updated cognito variables

--- pages/login.vue
+++ pages/login.vue
@@ -12,35 +12,33 @@ import { Amplify } from 'aws-amplify';
 // https://ui.docs.amplify.aws/vue/connected-components/authenticator/customization#headers--footers
 // if (process.env.NUXT_PUBLIC_AMPLIFY_TEST === undefined) {
 
-if (process.env.NUXT_PUBLIC_AMPLIFY_TEST === undefined) {
+if (import.meta.env.VITE_AMPLIFY_TEST === undefined) {
   // console.log("NUXT_PUBLIC_AMPLIFY_TEST: " + process.env.NUXT_PUBLIC_AMPLIFY_TEST);
   // console.log("AMPLIfy_TEST:" + process.env.AMPLIFY_TEST);
   // console.log("VUE_APP_AMPLIFY_TEST: ", process.env.VUE_APP_AMPLIFY_TEST);
-  console.log("VITE Variable: ", import.meta.env.VITE_AMPLIFY_TEST)
+  console.log("VITE Variable Not found!")
   // console.log("VITE Variable2: ", process.env.VITE_AMPLIFY_TEST)
-
-
   throw new Error("Missing TEST environment variable");
 } else {
-  console.log("TEST environment variable is set to:", process.env.NUXT_PUBLIC_AMPLIFY_TEST);
+  console.log("TEST environment variable is set to:", import.meta.env.VITE_AMPLIFY_TEST);
 }
-if (process.env.AMPLIFY_COGNITO_USER_POOL_ID === undefined) {
+if (import.meta.env.VITE_COGNITO_USER_POOL_ID === undefined) {
   throw new Error("Missing COGNITO_USER_POOL_ID environment variable");
 }
 
-if (process.env.COGNITO_USER_POOL_CLIENT_ID === undefined) {
+if (import.meta.env.VITE_COGNITO_USER_POOL_CLIENT_ID === undefined) {
   throw new Error("Missing COGNITO_USER_POOL_CLIENT_ID environment variable");
 }
-if (process.env.COGNITO_IDENTITY_POOL_ID === undefined) {
+if (import.meta.env.VITE_COGNITO_IDENTITY_POOL_ID === undefined) {
   throw new Error("Missing COGNITO_IDENTITY_POOL_ID environment variable");
 }
 
 Amplify.configure({
     Auth: {
     Cognito: {
-      userPoolId: process.env.AMPLIFY_COGNITO_USER_POOL_ID,
-      userPoolClientId: process.env.COGNITO_USER_POOL_CLIENT_ID,
-      identityPoolId: process.env.COGNITO_IDENTITY_POOL_ID, 
+      userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
+      userPoolClientId: import.meta.env.VITE_COGNITO_USER_POOL_CLIENT_ID,
+      identityPoolId: import.meta.env.VITE_COGNITO_IDENTITY_POOL_ID, 
       loginWith: {
         email: true,
       },
