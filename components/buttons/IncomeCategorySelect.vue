64452591fe301f7f0002ff503ab215db1e53a60a
kingchappers <sam.chapman@outlook.com>
2025-09-11 08:27:17 +0100

    testing selectbox component

---
+++ components/buttons/IncomeCategorySelect.vue
@@ -0,0 +1,23 @@
+<!-- Vue component -->
+<script>
+  import Multiselect from 'vue-multiselect'
+
+  export default {
+    components: { Multiselect },
+    data () {
+      return {
+        value: null,
+        options: ['Gift', 'Job', 'Other', 'Refund', 'Rent', 'Side Project', 'Tax Refund']
+      }
+    }
+  }
+</script>
+
+<template>
+  <div>
+    <multiselect v-model="value" :options="options"></multiselect>
+  </div>
+</template>
+
+<!-- Add Multiselect CSS. Can be added as a static asset or inside a component. -->
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
