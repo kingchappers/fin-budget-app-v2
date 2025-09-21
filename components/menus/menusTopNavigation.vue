48a31aeac398f5306d57d3ea4945ea145a7b2733
kingchappers <sam.chapman@outlook.com>
2025-09-21 21:20:45 +0100

    updated colours

--- components/menus/menusTopNavigation.vue
+++ components/menus/menusTopNavigation.vue
@@ -77,18 +77,13 @@ const links = [
 </script>
 
 <template>
-    <div class="lg:flex md:flex-nowrap border-b border-gray-200 dark:border-gray-800 color:black w-lvw">
-        <UHorizontalNavigation :links="links" color="blue" />
+    <div class="lg:flex md:flex-nowrap border-b border-gray-200 dark:border-gray-800 w-lvw">
+        <UHorizontalNavigation :links="links"/>
         <UDropdown :items="utilityMenu" :popper="{ placement: 'bottom-start' }">
             <UButton label="Utilities" icon="i-heroicons-bars-3-20-solid" color="white" variant="soft" class="ml-2" />
         </UDropdown>
         <UDropdown :items="accountMenuToUse" :popper="{ placement: 'bottom-start' }">
             <UButton label="Account" icon="i-heroicons-bars-3-20-solid" color="white" variant="soft" class="ml-2" />
         </UDropdown>
-        <!-- <UButton variant="soft" color="white" icon="i-heroicons-arrow-left-start-on-rectangle"
-            class="my-5 bg-transparant border-transparent text-gray-500 hover:bg-white hover:text-gray-600"
-            @click="auth.signOut">
-            Sign Out
-        </UButton> -->
     </div>
 </template>
