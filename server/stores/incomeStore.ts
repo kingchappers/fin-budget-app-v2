66ded59a5da62b71a266d05a77beb75e338b09f7
kingchappers <sam.chapman@outlook.com>
2025-10-12 20:57:50 +0100

    updated fetch call

--- server/stores/incomeStore.ts
+++ server/stores/incomeStore.ts
@@ -4,6 +4,7 @@ import type { incomeFilter } from '~/types/incomeFilter';
 import type { incomeType } from "~/types/incomeTypes";
 import { useUserStore } from "./userStore";
 import { useAuthenticator } from '@aws-amplify/ui-vue';
+import { useFetch } from "nuxt/app";
 
 const incomeFilter: incomeFilter = {
     limit: 5,
@@ -22,28 +23,28 @@ export const useIncomeStore = defineStore('incomeStore', {
     actions: {
         async fetch() {
             const session = await fetchAuthSession();
-            let authorisation = ''
-            if (session.tokens && session.tokens.idToken) {
-                authorisation = session.tokens.idToken.toString()
-            } else {
-                console.log('Error: Session token not found. Redirecting to login')
-            }
+            // let authorisation = ''
+            // if (session.tokens && session.tokens.idToken) {
+            //     authorisation = session.tokens.idToken.toString()
+            // } else {
+            //     console.log('Error: Session token not found. Redirecting to login')
+            // }
 
-            const userStore = useUserStore();
-            incomeFilter.userId = userStore.userId;
-            console.log(incomeFilter)
-            const incomeList = await $fetch('/api/incomes/getIncomes', {
-                method: 'get',
-                headers: {
-                    Authorisation: authorisation,
-                    UserId: userStore.userId
-                },
-                params: {
-                    incomeFilter
-                },
-            })
-            console.log(incomeList)
-            this.incomeList = incomeList || {}
+            // const userStore = useUserStore();
+            // incomeFilter.userId = userStore.userId;
+            // console.log(incomeFilter)
+            // const incomeList = await $fetch('/api/incomes/getIncomes', {
+            //     method: 'get',
+            //     headers: {
+            //         Authorisation: authorisation,
+            //         UserId: userStore.userId
+            //     },
+            //     params: {
+            //         incomeFilter
+            //     },
+            // })
+            // console.log(incomeList)
+            // this.incomeList = incomeList || {}
 
 
 
@@ -58,7 +59,7 @@ export const useIncomeStore = defineStore('incomeStore', {
             } else {
                 console.log('Error: Session token not found. Redirecting to login')
             }
-            const newIncomeList = await $fetch('https://dg2rxkailb.execute-api.eu-west-2.amazonaws.com/prod/income', {
+            const newIncomeList = await useFetch('https://dg2rxkailb.execute-api.eu-west-2.amazonaws.com/prod/income', {
                 method: 'GET',
                 headers: {
                     'Authorization': token,
